e368e080e29f813dba1d9fd71dd350400fa41a6b
Eduardo Canellas de Oliveira <eduardo.canellas@bemobi.com>
2022-12-03 15:07:26 -0300

    finish day one

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -2,21 +2,24 @@ fn main() -> eyre::Result<()> {
     let input = std::fs::read_to_string("input/day-1.txt")?;
 
     let mut acc = 0;
-    let mut most_calories = 0;
+
+    let mut elves: Vec<i32> = Vec::new();
 
     for line in input.lines() {
         match line.trim().parse::<i32>() {
             Ok(number) => acc += number,
             Err(_) => {
-                if acc > most_calories {
-                    most_calories = acc;
-                }
+                elves.push(acc);
                 acc = 0;
             }
         }
     }
 
-    println!("{most_calories}");
+    elves.sort();
+
+    let top_calories: i32 = elves.iter().rev().take(3).sum();
+
+    println!("{top_calories}");
 
     Ok(())
 }
